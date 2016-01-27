62c405d7a48da4f9563a02a41dfbc5f7b58f3602
Xinjiang Shao <shaoxinjiang@gmail.com>
2016-01-27 15:38:29 -0600

    add another demo email address

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -150,7 +150,7 @@ INSERT IGNORE INTO `emails` (
     `userId`, `value`, `type`, `isPrimary`
 )VALUES
 ('2819c223-7f76-453a-919d-413861904646', 'shaoxinjiang@gmail.com' ,'work', true),
-('2819c223-7f76-453a-919d-413861904646', 'shaoxinjiang@gmail.com' ,'work', true);
+('2819c223-7f76-453a-919d-413861904646', 'xinjiang.shao@gmail.com' ,'personal', false);
 
 INSERT IGNORE INTO `groups` (
     `id`, `displayName`
