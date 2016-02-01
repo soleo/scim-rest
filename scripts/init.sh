28d95071d94e820791a455286ed4ba15701bbc3e
Xinjiang Shao <shaoxinjiang@gmail.com>
2016-01-31 22:26:18 -0600

    change testing db again

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mysql.server restart
 
 MYSQL_HOST=127.0.0.1
+PWD=`pwd`
 
-mysql -h $MYSQL_HOST -u root < db_clean.sql
+mysql -h $MYSQL_HOST -u root < $PWD/db_clean.sql
 
-mysql -h $MYSQL_HOST -u root < db_init.sql
+mysql -h $MYSQL_HOST -u root < $PWD/db_init.sql
 
-mysql -h $MYSQL_HOST -u scim_test -p1e0OZH5f9asCJ0Anj11ia0Wmy scim_test < db_schema.sql
+mysql -h $MYSQL_HOST -u scim_test -p1e0OZH5f9asCJ0Anj11ia0Wmy scim_test < $PWD/db_schema.sql
 
 mysql -h $MYSQL_HOST -u scim_test -p1e0OZH5f9asCJ0Anj11ia0Wmy scim_test -e 'show tables;'
