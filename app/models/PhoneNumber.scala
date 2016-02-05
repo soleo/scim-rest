fddc158849ccef302fcf7b7deb242c931787f48b
Xinjiang Shao <shaoxinjiang@gmail.com>
2016-02-04 22:12:31 -0600

    Reformat PhoneNumber class

--- app/models/PhoneNumber.scala
+++ app/models/PhoneNumber.scala
@@ -5,11 +5,10 @@ import play.api.libs.json._
 import play.api.data.validation.ValidationError
 
 object PhoneNumber {
-    implicit val phoneNumberFormat = Json.format[PhoneNumber]
+  implicit val phoneNumberFormat = Json.format[PhoneNumber]
 }
 
-case class PhoneNumber(    
-    override val value: Option[String] = None,
-    override val `type`: Option[String] = None,
-    override val primary: Option[Boolean] = None
-) extends PluralAttribute
+case class PhoneNumber(
+  override val value: Option[String] = None,
+  override val `type`: Option[String] = None,
+  override val primary: Option[Boolean] = None) extends PluralAttribute
